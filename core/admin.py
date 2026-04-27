6ede4f8a81a295e52b3d2b573e01a69b66378ced
Matheus Pinheiro <mpdev34@gmail.com>
2026-04-27 19:12:05 -0300

    corrigindo o amdin.py

--- core/admin.py
+++ core/admin.py
@@ -23,38 +23,38 @@ class AvaliacaoFisicaAdmin(admin.ModelAdmin):
     readonly_fields = (
     'criado_em',
     )
-fieldsets = (
-    ('Dados Pessoais', {
-        'fields': ('nome', 'sexo', 'data_nascimento')
-    }),
-    ('Dados Antropométricos', {
-        'fields':('altura', 'peso'),
-        'classes': ('wide',),
-    }),
-    ('Dobras cutâneas (mm)',{
-        'fields': (
-            'triceps', 'subescapular', 'peitoral',
-            'axilar_media', 'supra_iliaca', 
-            'abdome_dobra', 'coxa_dobra'
-        )
-    }),
-    ('Circunferência (cm)', {
-        'fields': (
-            'ombro', 'torax', 'cintura', 'abdome_circ',
-            'quadril', 'braco_e', 'braco_d', 'coxa_e', 'coxa_d',
-            'panturrilha_e', 'panturrilha_d',
-        )
-    }),
-    ('Resultados', {
-        'fields': (
-            'percentual_gordura_formatado',
-            'massa_gorda_formatado', 
-            'massa_magra_formatado',
+    fieldsets = (
+        ('Dados Pessoais', {
+            'fields': ('nome', 'sexo', 'data_nascimento')
+        }),
+        ('Dados Antropométricos', {
+            'fields':('altura', 'peso'),
+            'classes': ('wide',),
+        }),
+        ('Dobras cutâneas (mm)',{
+            'fields': (
+                'triceps', 'subescapular', 'peitoral',
+                'axilar_media', 'supra_iliaca', 
+                'abdome_dobra', 'coxa_dobra'
+            )
+        }),
+        ('Circunferência (cm)', {
+            'fields': (
+                'ombro', 'torax', 'cintura', 'abdome_circ',
+                'quadril', 'braco_e', 'braco_d', 'coxa_e', 'coxa_d',
+                'panturrilha_e', 'panturrilha_d',
+            )
+        }),
+        ('Resultados', {
+            'fields': (
+                'percentual_gordura_formatado',
+                'massa_gorda_formatado', 
+                'massa_magra_formatado',
 
-        )
+            )
 
-    }),
-)
+        }),
+    )
 admin.site.register(AvaliacaoFisica, AvaliacaoFisicaAdmin)
 
 class VariacaoInline(admin.TabularInline):
