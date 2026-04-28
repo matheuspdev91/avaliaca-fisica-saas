6bc3c17b6dcaeb8ebbba0dc370646f246d43d7ad
Matheus Pinheiro <mpdev34@gmail.com>
2026-04-28 10:06:45 -0300

    testando o decortarator

--- core/decorators.py
+++ core/decorators.py
@@ -2,22 +2,22 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import user_passes_test
 
 
-def eh_aluno(user):
-    return hasattr(user, 'aluno')
+#def eh_aluno(user):
+   #return hasattr(user, 'aluno')
 
 
-def eh_personal(user):
-    return not hasattr(user, 'aluno')
+#def eh_personal(user):
+ #   return not hasattr(user, 'aluno')
 
-def apenas_aluno(view_func):
-    return user_passes_test(
-        eh_aluno,
-        login_url ='core:login'
-    )(view_func)
+#def apenas_aluno(view_func):
+ #   return user_passes_test(
+  #      eh_aluno,
+   #     login_url ='core:login'
+    #)(view_func)
 
-def apenas_personal(view_func):
-    def wrapper(request, *args, **kwargs):
-        if hasattr(request.user, 'aluno'):
-            return redirect('core:painel_aluno', aluno_id=request.user.aluno.id)
-        return view_func(request, *args, **kwargs)
-    return wrapper
+#def apenas_personal(view_func):
+ #   def wrapper(request, *args, **kwargs):
+  #      if hasattr(request.user, 'aluno'):
+   #         return redirect('core:painel_aluno', aluno_id=request.user.aluno.id)
+    #    return view_func(request, *args, **kwargs)
+    #return wrapper
