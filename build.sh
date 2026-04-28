4954ed2267c0584b01a3e149c27daf5825d052c1
Matheus Pinheiro <mpdev34@gmail.com>
2026-04-28 16:11:36 -0300

    subindo o seed.py

--- build.sh
+++ build.sh
@@ -4,8 +4,13 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
-export PYTHON_VERSION=3.11.9
+
 
 
 # CRIAR SUPERUSER AUTOMATICO
 python manage.py createsuperuser --noinput || true
+
+
+# SEED.PY
+
+python manage.py seed
