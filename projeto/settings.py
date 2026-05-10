53545ec39fd70ffd04a81e6f4b3a3e07b8b2b236
Matheus Batista <mpdev34@gmail.com>
2026-05-10 13:11:54 -0300

    fix da settings

--- projeto/settings.py
+++ projeto/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = "projeto.wsgi.application"
 # =========================
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=True,
     )
 }
 
