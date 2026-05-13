6f8b4663edae1f1c164c222bc03e7443ea1fe567
Matheus Batista <mpdev34@gmail.com>
2026-05-13 09:11:32 -0300

    remove duplicação

--- projeto/settings.py
+++ projeto/settings.py
@@ -167,12 +167,6 @@ MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
-    secure=True,
-)
 
 # =========================
 # LOGIN / LOGOUT
